f9ad4d0ea052e0b4476a2f5ba010c8c49b6d49a9
DarDog <samediacc666@gmial.com>
2021-12-27 18:39:19 +1100

    Feat: add layout of Search form

---
+++ src/components/SearchForm/SearchForm.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './SearchForm.css'
+
+const SearchForm = () => {
+  return(
+    <section className="search main__search">
+      <form className="search__form">
+        <input type="text" className="search__input" placeholder="Фильм"/>
+        <button type="submit" className="search__submit">Найти</button>
+      </form>
+      <div className='search__switch-container'>
+      <label className="search__switch">
+        <input className="search__checkbox" type="checkbox"/>
+        <span className="search__slider"/>
+      </label>
+      <p className='search__paragraph'>Короткометражки</p>
+      </div>
+    </section>
+  );
+}
+
+export default SearchForm;
