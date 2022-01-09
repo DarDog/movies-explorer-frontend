985d14390c88e6c26efed167fd3f9d3fe518ea27
DarDog <samediacc666@gmial.com>
2022-01-09 20:44:53 +1100

    Feat: update snapshots

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -3,10 +3,10 @@ import './Promo.css';
 
 const Promo = () => {
   return(
-    <div className='promo main__promo'>
+    <section className='promo main__promo'>
       <div className="promo__background-logo" />
       <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
-    </div>
+    </section>
   );
 }
 
