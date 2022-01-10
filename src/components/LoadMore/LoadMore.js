f579c5fd3cb948ec2852b0755e2e745d117c96f4
DarDog <samediacc666@gmial.com>
2022-01-10 16:06:32 +1100

    Feat: Add LoadMore component

---
+++ src/components/LoadMore/LoadMore.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './LoadMore.css'
+
+const LoadMore = () => {
+  return(
+    <section className='load-more'>
+      <button type='button' className='load-more__button'>Ещё</button>
+    </section>
+  );
+}
+
+export default LoadMore;
--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import './MoviesCardList.css'
 import MoviesCard from "../MoviesCard/MoviesCard";
+import LoadMore from "../LoadMore/LoadMore";
 
 const MoviesCardList = ({ movies = [], ...props }) => {
   return (
-    <section className='movies main__movies'>
-      <ul className='movies__list'>
-        {
-          movies.map(movie => {
-            return (
-              <MoviesCard key={movie._id} movie={movie} isSaves={props.isSaves} />
-            );
-          })
-        }
-      </ul>
-    </section>
+    <>
+      <section className='movies main__movies'>
+        <ul className='movies__list'>
+          {
+            movies.map(movie => {
+              return (
+                <MoviesCard key={ movie._id } movie={ movie } isSaves={ props.isSaves }/>
+              );
+            })
+          }
+        </ul>
+      </section>
+      <LoadMore/>
+    </>
   );
 }
 
