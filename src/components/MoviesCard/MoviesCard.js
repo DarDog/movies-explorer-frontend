6b3b38a1c81e3f0057b240fcab4852b4653d638a
DarDog <samediacc666@gmial.com>
2022-01-14 01:16:20 +1100

    Feat: add alt for images

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import './MovieCard.css';
 
-const MoviesCard = ({ movie, ...props}) => {
+const MoviesCard = ({ movie, ...props }) => {
   return (
     <li className='movies__item'>
       <div className="movies__info">
         <h2 className="movies__title">{ movie.title }</h2>
         <p className="movies__duration">{ Math.floor(movie.duration / 60) }ч { movie.duration % 60 }м</p>
-        {props.isSaves
+        { props.isSaves
           ? <button type='button' className={ `movies__delete` }/>
           : <button type='button' className={ `movies__like ${ movie.isLiked && 'movies__like_active' }` }/>
         }
       </div>
-      <img src={ movie.image } alt="Заглушка" className="movies__poster"/>
+      <img src={ movie.image } alt={ movie.title } className="movies__poster"/>
     </li>
   );
 }
