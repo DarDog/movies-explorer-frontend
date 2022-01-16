916fa5b6fdc1807a2797d4afccff359500c4a263
DarDog <samediacc666@gmial.com>
2022-01-16 22:17:44 +1100

    Fix with review

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -8,15 +8,15 @@ const Portfolio = () => {
       <h3 className='portfolio__title'>Портфолио</h3>
       <Ul isColumn={true} anotherClass='portfolio__list'>
         <li className='portfolio__item'>
-          <a className='portfolio__link' href="https://dardog.github.io/how-to-learn/">Статичный сайт</a>
+          <a className='portfolio__link' target='_blank' href="https://dardog.github.io/how-to-learn/">Статичный сайт</a>
           <div className='link__arrow'/>
         </li>
         <li className='portfolio__item'>
-          <a className='portfolio__link' href="https://dardog.github.io/russian-travel/">Адаптивный сайт</a>
+          <a className='portfolio__link' target='_blank' href="https://dardog.github.io/russian-travel/">Адаптивный сайт</a>
           <div className='link__arrow'/>
         </li>
         <li className='portfolio__item'>
-          <a className='portfolio__link' href="http://mesto.subb.nomoredomains.rocks/sign-in">Одностраничное приложение</a>
+          <a className='portfolio__link' target='_blank' href="http://mesto.subb.nomoredomains.rocks/sign-in">Одностраничное приложение</a>
           <div className='link__arrow'/>
         </li>
       </Ul>
